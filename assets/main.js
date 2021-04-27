a5942fa2b692f1055eadaaa3bf3218c879c66722
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-27 18:11:38 +0200

    add list on document

--- assets/main.js
+++ assets/main.js
@@ -2,17 +2,21 @@
 for(var i=1; i<101; i++){
     //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
     if(i% 3==0 && i% 5==0 ){
-            console.log("FizzBuzz");
-        }
+        console.log("FizzBuzz");
+        document.getElementById("lista_numeri").innerHTML +='<li>' + 'FizzBuzz' + '</li>';
+    }
     //ma per i multipli di 3 stampi “Fizz” al posto del numero 
     else if(i % 3 == 0){
         console.log("Fizz");
+        document.getElementById("lista_numeri").innerHTML += '<li>' + 'Fizz' + '</li>';
     }
     //e per i multipli di 5 stampi Buzz.
     else if(i % 5 == 0){
         console.log("Buzz");
+        document.getElementById("lista_numeri").innerHTML += '<li>' + 'Buzz' + '</li>';
     }
     else{
         console.log(i);
+        document.getElementById("lista_numeri").innerHTML +=  '<li>' + i + '</li>';
     }
 }
