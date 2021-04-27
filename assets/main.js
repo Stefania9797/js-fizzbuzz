a4bf4e54b0410cffd85d5d7bb654c7063f713ff8
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-27 14:15:44 +0200

    complete ex

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,18 @@
 //Scrivi un programma che stampi i numeri da 1 a 100,
 for(var i=1; i<101; i++){
-    if(i % 3 == 0){
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    if(i% 3==0 && i% 5==0 ){
+            console.log("FizzBuzz");
+        }
+    //ma per i multipli di 3 stampi “Fizz” al posto del numero 
+    else if(i % 3 == 0){
         console.log("Fizz");
     }
+    //e per i multipli di 5 stampi Buzz.
     else if(i % 5 == 0){
         console.log("Buzz");
     }
+    else{
         console.log(i);
-    
+    }
 }
-//ma per i multipli di 3 stampi “Fizz” al posto del numero 
-//e per i multipli di 5 stampi Buzz.
-//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
